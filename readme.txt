6d57b4f361aa2d4fe7244c7449eb2c5f28166ca3
bryght <bryght@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-13 10:54:40 +0000

    Minor bug fixes
    git-svn-id: https://plugins.svn.wordpress.org/wp-booking-system/tags/1.2@1024994 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,131 @@
+=== WP Booking System ===
+Contributors: veribo
+Tags: booking system, booking calendar, booking engine, booking, system, calendar, engine, bookings, bookable, reservation calendar, reservations, scheduling, schedule, rooms, hotel, holiday home, accommodations, dateblocker, date blocker, bed and breakfast
+Requires at least: 3.0
+Tested up to: 4.0
+Stable tag: 1.2
+
+Booking system for WordPress. Bookings made easy! Easy to configure and customize.
+
+== Description ==
+
+WP Booking System is a simple booking system for WordPress. You will be up and running in just a few minutes. Start receiving bookings from your visitors today! You can create booking calendars and forms, and you can manage your bookings. You can easily customize the booking system to fit your needs!
+
+Download the Premium version at: http://www.wpbookingsystem.com
+
+Features of the Free version:
+
+* Create your own booking system: a booking calendar and a booking form!
+* Receive and manage bookings
+* Save extra booking information
+* Generate a shortcode to insert the booking calendar anf booking form into a page or post
+* WP Booking System Widget
+* Supports multiple languages
+
+Extra features of the Premium version:
+
+* Create an unlimited number of booking calendars
+* Create an unlimited number of booking forms
+* Display multiple months
+* Create your own legend: apply your own colors and text
+* Change the first day of the week
+* Change the start month / year
+* Edit multiple dates with just one click
+* Very easy to translate into any language
+* Download the Premium version at: http://www.wpbookingsystem.com
+
+== Installation ==
+
+1. Install the plugin by uploading the zip file (Plugins > Add New > Upload)
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to the settings page of the plugin to setup a calendar
+4. Embed the calendar on any page or post using the Add Calendar button
+
+A WP Booking System widget is also available.
+
+
+== Frequently Asked Questions ==
+
+= How can I embed the booking calendar on a page or post? =
+
+Use the 'Add Calendar' button above the editor.
+
+= Will this booking system work for me? =
+
+We’re pretty sure about that! We made this plugin as 'flexible' as possible. The booking plugin works with bookings on a per day basis (no time slots). So if you rent something out on a daily basis, then the WP Booking System is a good choice.
+
+= I have another question =
+
+Please see http://www.wpbookingsystem.com for more information and ask your questions there!
+
+
+== Screenshots ==
+
+1. WP Booking System inserted into a page
+2. Receive and manage bookings
+
+
+== Changelog ==
+
+= 1.2 =
+* Minor bug fixes
+
+= 1.1 =
+* UTF-8 fix
+
+= 1.0 =
+* Changed the version number to 1.0
+
+= 0.7 =
+* Small CSS fixes and a language fix related to the widget
+
+= 0.6 =
+* Second output buffer fix
+
+= 0.5 =
+* Output buffer fix
+
+= 0.4 =
+* Fix in booking details fields
+
+= 0.3 =
+* Minor fix
+
+= 0.2 =
+* Small CSS fixes
+
+= 0.1 =
+* First release
+
+
+== Upgrade Notice ==
+
+= 1.2 =
+* Minor bug fixes
+
+= 1.1 =
+* UTF-8 fix
+
+= 1.0 =
+* Changed the version number to 1.0
+
+= 0.7 =
+* Small CSS fixes and a language fix related to the widget
+
+= 0.6 =
+* Second output buffer fix
+
+= 0.5 =
+* Output buffer fix
+
+= 0.4 =
+* Fix in booking details fields
+
+= 0.3 =
+* Minor fix
+
+= 0.2 =
+* Small CSS fixes
+
+= 0.1 =
+* First release
---
+++ views/form/edit-form.php
@@ -0,0 +1,108 @@
+<?php global $wpdb;?>
+<div class="wrap wpbs-wrap">
+    <div id="icon-themes" class="icon32"></div>
+    <h2>Edit Form</h2>
+    <?php if(!empty($_GET['save']) && $_GET['save'] == 'ok'):?>
+    <div id="message" class="updated">
+        <p><?php echo __('The form was updated','wpbs')?></p>
+    </div>
+    <?php endif;?>
+    <?php if(!(!empty($_GET['id']))) $_GET['id'] = 'wpbs-new-form';?>
+    <?php $sql = $wpdb->prepare('SELECT * FROM ' . $wpdb->prefix . 'bs_forms WHERE formID=%d',$_GET['id']); ?>
+    <?php $form = $wpdb->get_row( $sql, ARRAY_A );?>
+    <?php if($wpdb->num_rows > 0 || $_GET['id'] == 'wpbs-new-form'): $formOptions = json_decode($form['formOptions'],true);?>
+        <div class="postbox-container meta-box-sortables">
+            
+            <form action="<?php echo admin_url( 'admin.php?page=wp-booking-system-forms&do=save-form&noheader=true');?>" method="post">
+            <div class="wpbs-buttons-wrapper">
+                <input type="submit" class="button button-primary button-h2 saveCalendar" value="<?php echo __("Save Changes",'wpbs') ;?>" /> 
+                <a class="button secondary-button button-h2 button-h2-back-margin" href="<?php echo admin_url( 'admin.php?page=wp-booking-system-forms' );?>">Back</a>
+            </div>
+            <input type="text" name="formTitle" class="fullTitle" id="formTitle" placeholder="<?php echo __("Form title",'wpbs') ;?>" value="<?php echo esc_html(stripslashes(((!empty($form['formTitle'])) ? $form['formTitle'] : ""))) ;?>"/>
+            
+                
+            
+            <div class="metabox-holder">
+                    <?php wpbs_edit_form( array('formData' => $form['formData']) );?>
+                    <input type="hidden" value="<?php echo (!empty($form['formID'])) ? $form['formID'] : "" ;?>" name="formID" />   
+            </div> 
+            <div class="metabox-holder">
+                <div class="postbox closed">
+                    <div class="handlediv" title="Click to toggle"><br /></div>
+                    <h3 class="hndle">Form Settings</h3>
+                    <div class="inside">
+                        <div class="wpbs-settings-col">
+                            <div class="wpbs-settings-col-left">
+                                <strong>Receive messages by mail</strong>                                
+                            </div>
+                            <div class="wpbs-settings-col-right">
+                                <select name="receive_emails" id="receive_emails">
+                                    <option value="yes">Yes</option>
+                                    <option value="no"<?php if(empty($formOptions['sendTo'])):?> selected="selected"<?php endif;?>>No</option>
+                                </select>
+                            </div>
+                            <div class="wpbs-clear"></div>                            
+                        </div>
+                        
+                        <div class="wpbs-settings-col" id="send_to_emails" <?php if(empty($formOptions['sendTo'])):?>style="display:none;"<?php endif;?>>
+                            <div class="wpbs-settings-col-left">
+                                <strong>Send to:</strong>                                
+                            </div>
+                            <div class="wpbs-settings-col-right">
+                                <input type="text" name="sendto" value="<?php echo esc_html($formOptions['sendTo']);?>" />
+                                <small>Separate multiple e-mail addresses with a comma</small>
+                            </div>
+                            <div class="wpbs-clear"></div>                            
+                        </div>
+                        
+                        <div class="wpbs-settings-col">
+                            <div class="wpbs-settings-col-left">
+                                <strong>Confirmation message:</strong>                                
+                            </div>
+                            <div class="wpbs-settings-col-right">
+                                <textarea class="widefat" name="confirmation"><?php echo esc_html($formOptions['confirmationMessage']);?></textarea>
+                                <small>The message to be shown to the visitor when the form was submitted succesfully</small>
+                            </div>
+                            <div class="wpbs-clear"></div>                            
+                        </div>    
+                    </div>
+                </div>
+            </div> 
+            
+            <div class="metabox-holder">
+                <div class="postbox closed">
+                    <div class="handlediv" title="Click to toggle"><br /></div>
+                    <h3 class="hndle">Submit Button</h3>
+                    <div class="inside">
+                        <div class="wpbs-settings-col">
+                            <div class="wpbs-settings-col-left">
+                                <strong>Default Label</strong>                                
+                            </div>
+                            <div class="wpbs-settings-col-right">
+                                <input type="text" name="submitLabel" value="<?php echo esc_html($formOptions['submitLabel']['default']);?>" />
+                            </div>
+                            <div class="wpbs-clear"></div>                            
+                        </div>
+                        <hr />
+                        <?php $activeLanguages = json_decode(get_option('wpbs-languages'),true); foreach ($activeLanguages as $code => $language):?>
+                        <div class="wpbs-settings-col">
+                            <div class="wpbs-settings-col-left">
+                                <strong><?php echo $language;?></strong>                                
+                            </div>
+                            <div class="wpbs-settings-col-right">
+                                <input type="text" name="submitLabel_<?php echo $code;?>" value="<?php echo esc_html($formOptions['submitLabel'][$code]);?>" />
+                            </div>
+                            <div class="wpbs-clear"></div>                            
+                        </div>
+                        <?php endforeach;?>
+                    </div>
+                </div>
+            </div> 
+            <input type="submit" class="button button-primary saveCalendar" style="margin-top: 20px;" value="Save Changes" /> 
+            </form>
+        </div>
+    <?php else:?>
+        <?php echo __('Invalid calendar ID.','wpbs')?>
+    <?php endif;?>     
+</div>
+
